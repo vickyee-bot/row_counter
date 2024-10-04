ac0c60e015f62f33326f4bd61ecf3b8a376257d8
Peter Wanjiru <121359709+vickyee-bot@users.noreply.github.com>
2024-10-04 08:31:06 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// document.getElementById("count").innerText = 5
+
+
+//let count = 0
+//console.log(count)
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+  let countStr = "" + count + " - "
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
+}
+
